3e6628ed237526bca10d0d0dd45bc3e75347b58f
Heikki Pesonen <heikki.pesonen@luotta.fi>
2014-11-12 19:00:01 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name musakornerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the musakornerApp
+ */
+angular.module('musakornerApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name musakornerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the musakornerApp
+ */
+angular.module('musakornerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
