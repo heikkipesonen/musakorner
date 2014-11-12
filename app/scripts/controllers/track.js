de2f1d79942c2f7afab83d836aeb9f0fe136e9a7
Heikki Pesonen <heikki.pesonen@luotta.fi>
2014-11-12 21:24:21 +0200

    adjusted css things

--- app/scripts/controllers/track.js
+++ app/scripts/controllers/track.js
@@ -8,10 +8,22 @@
  * Controller of the musakornerApp
  */
 angular.module('musakornerApp')
-  .controller('TrackCtrl',['$scope','api', function ($scope, api) {
+  .controller('TrackCtrl',['$scope','api','$timeout', function ($scope, api, $timeout) {
   	angular.extend($scope, {
+  		busy:false,
   		castVote:function(){
-  			api.castVote($scope.ngModel.id);
+  			$scope.busy = true;
+  			api.castVote($scope.ngModel.id).then(function(response){
+
+  				console.info(response);
+
+  			}, function(){
+  				
+  				$timeout(function(){
+  					$scope.busy = false;
+  					console.log('joo');
+  				},2000);
+  			});
   		}
   	});
   }]);
