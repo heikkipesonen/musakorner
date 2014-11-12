41be48e926184481264707b8961d4eed5702af18
Heikki Pesonen <heikki.pesonen@luotta.fi>
2014-11-12 20:08:58 +0200

    musa

--- test/spec/controllers/playlist.js
+++ test/spec/controllers/playlist.js
@@ -1,17 +1,17 @@
 'use strict';
 
-describe('Controller: MainCtrl', function () {
+describe('Controller: PlaylistCtrl', function () {
 
   // load the controller's module
   beforeEach(module('musakornerApp'));
 
-  var MainCtrl,
+  var PlaylistCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    MainCtrl = $controller('MainCtrl', {
+    PlaylistCtrl = $controller('PlaylistCtrl', {
       $scope: scope
     });
   }));
